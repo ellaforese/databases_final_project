5e072f31b09a9362098b9af35b8e8efdde3d2951
Cole <cpleasan@villanova.edu>
2024-04-24 18:12:02 -0400

    changed queries

--- basketball-files/basketball-queries.sql
+++ basketball-files/basketball-queries.sql
@@ -14,20 +14,19 @@ SELECT *
 FROM Team
 WHERE division_id = 'B';
 
-
 SELECT
-    CASE
-        WHEN g.Home_Score > g.Away_Score THEN g.Home
-        WHEN g.Away_Score > g.Home_Score THEN g.Away
-        ELSE NULL
-    END AS Winning_ID,
-    t.TeamName AS Winning_Team
+    t.TeamName AS Winning_Team_Name,
+    g.Game_Name   
 FROM
     Game g
 LEFT JOIN
-    Team t ON
+    Team t ON 
     CASE
         WHEN g.Home_Score > g.Away_Score THEN g.Home
         WHEN g.Away_Score > g.Home_Score THEN g.Away
-        ELSE NULL
+        ELSE NULL 
     END = t.Team_ID;
+
+
+
+
