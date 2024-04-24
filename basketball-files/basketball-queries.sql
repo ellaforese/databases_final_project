db9e196557b1342031efeb092e22d71b50499a9b
Cole <cpleasan@villanova.edu>
2024-04-24 17:25:54 -0400

    changed queries

---
+++ basketball-files/basketball-queries.sql
@@ -0,0 +1,15 @@
+SELECT * 
+FROM Player 
+WHERE Skill_Level = 'NOV';
+
+SELECT * 
+FROM Player 
+WHERE Skill_Level = 'ABV';
+
+SELECT * 
+FROM Team
+WHERE division_id = 'A';
+
+SELECT * 
+FROM Team
+WHERE division_id = 'C';
